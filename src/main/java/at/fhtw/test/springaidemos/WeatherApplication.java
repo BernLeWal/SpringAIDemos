1af723a7eb93f11e2f5338adba13a5b4b3759c5b
wallisch <walliscb@technikum-wien.at>
2024-06-12 11:48:34 +0200

    added WeatherApplication

---
+++ src/main/java/at/fhtw/test/springaidemos/WeatherApplication.java
@@ -0,0 +1,44 @@
+package at.fhtw.test.springaidemos;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner contextClr(ChatClient chatClient) {
+		return args -> {
+			String answer = chatClient.prompt()
+					.system("""
+							Current Weather
+							10:48 AM
+							13 °C
+							Cloudy with some rain
+							RealFeel 11 °C
+							Wind Gusts 29 km/h
+							Humidity 78 %
+							Dew Point -1 °C
+							Pressure 1011 mb
+							Cloud Cover 62 %
+							Visibility 12 km
+							Cloud Ceiling 10200 m""")
+					.user("What is the today's temperature?")
+					.call()
+					.content();
+			System.out.printf("\nChatGPT answered: \n\n%s\n", answer);
+		};
+	}
+}
